ae2ce0756cb27caf8de9f1677fe5f437edf4919d
Andrey Sapeshko <andrdd17@gmail.com>
2025-07-05 17:50:04 +0300

    Создан модуль services с функцией analysis_categories_for_cashback

---
+++ src/services.py
@@ -0,0 +1,17 @@
+from tests.conftest import DATA_FROM_XLCX
+
+
+def analysis_categories_for_cashback(data: list[dict], year: str, month: str) -> str:
+    filtered_data = [x for x in data if x.get('Дата платежа') and x.get('Дата платежа')[3:] == f'{month}.{year}']
+    categories = set([x.get('Категория') for x in filtered_data if x.get('Категория')])
+    result = {}
+    for category in sorted(categories):
+        total_cashback = 0
+        for operation in filtered_data:
+            if operation.get('Сумма платежа') and operation.get('Категория') == category:
+                total_cashback += operation.get('Сумма платежа') / -100
+        result[category] = round(total_cashback, 2)
+    return result
+
+
+# print(analysis_categories_for_cashback(DATA_FROM_XLCX, '2021', '12'))
